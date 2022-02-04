e1d09ee914729d671f92d65b1deedbcc44f25146
CookieNyanCloud <emil8yunusov@gmail.com>
2022-02-04 19:03:37 +0300

    fix

--- webScraper/webScraper.go
+++ webScraper/webScraper.go
@@ -50,7 +50,7 @@ func NewScraper(conf *configs.Conf) IScraper {
 		lastNum:   114,
 		dif:       0,
 		lastNRNKO: 8,
-		nkoAll:    72,
+		nkoAll:    73,
 		nkoURL:    conf.NKOURL,
 		nkoBody:   conf.NKOBody,
 	}
